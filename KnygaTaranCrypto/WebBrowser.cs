a2e99bc9458e0cc0b0b975d7ad463faf1727565b
juline <juline-ua@yandex.ru>
2013-03-11 23:04:02 +0200

    Criptonator view edited. Added WebBrowser

---
+++ KnygaTaranCrypto/WebBrowser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using mshtml;
+
+namespace KnygaTaranCrypto
+{
+    public partial class WebBrowser : Form
+    {
+        private string _basicCSS = @"
+    /** {padding: 0; margin: 0}*/
+    body {overflow:none}
+";
+
+        public WebBrowser(string context)
+        {
+            InitializeComponent();
+            _webBrowser.DocumentText = context;
+        }
+
+        private void AddStyle(string style)
+        {
+            if (_webBrowser.Document != null)
+            {
+                IHTMLDocument2 doc = (_webBrowser.Document.DomDocument) as IHTMLDocument2;
+                IHTMLStyleSheet ss = doc.createStyleSheet("", 0);
+                ss.cssText = style;
+
+            }
+        }
+
+        private void _webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            this.Text = _webBrowser.Document.Title;
+            AddStyle(this._basicCSS);
+        }
+    }
+}
